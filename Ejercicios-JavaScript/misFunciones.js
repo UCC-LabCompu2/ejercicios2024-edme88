829b286d46b5422a18c6712be83db17c4525c1d9
EXB04891 <agustina.aliciardi@darwoft.com>
2024-05-22 10:05:10 -0300

    Se subio ejercicio de conversion de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,26 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function convertirUnidades(nombre, valor){
+
+    if(isNaN(valor)){
+        alert("El valor ingresado no es correcto");
+        document.getElementById("metro").value = "";
+        document.getElementById("pulgada").value = "";
+        document.getElementById("pie").value = "";
+        document.getElementById("yarda").value = "";
+    }else if(nombre==="metro"){
+        document.getElementById("pulgada").value = valor*39.3701;
+        document.getElementById("pie").value = valor*3.28084;
+        document.getElementById("yarda").value = valor*1.09361;
+    }else if(nombre==="pulgada"){
+        document.getElementById("metro").value = valor*0.0254;
+        document.getElementById("pie").value = valor*0.0833333;
+        document.getElementById("yarda").value = valor*0.0277778;
+    }else if(nombre==="pie"){
+        //TAREA
+    }else if(nombre==="yarda"){
+        //TAREA
+    }
+}
